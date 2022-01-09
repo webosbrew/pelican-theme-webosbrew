6d789e3f41f1a6f63dcfe6d4eb98f4fd5d386e42
Mariotaku <mariotaku.lee@gmail.com>
2022-01-10 02:32:10 +0900

    migrating setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,4 @@
-from setuptools import setup
+import setuptools
 
-setup(
-    name='pelican-theme-webosbrew',
-    version='1.0.0',
-    packages=[''],
-    url='https://github.com/webosbrew/pelican-theme-webosbrew',
-    license='Apache-2.0',
-    author='Ningyuan Li',
-    author_email='mariotaku.lee@gmail.com',
-    description='Pelican theme for webosbrew.org'
-)
+if __name__ == "__main__":
+    setuptools.setup()
