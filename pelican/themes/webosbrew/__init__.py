05dfdc05accca95a772fbc8362c71335f483c9e8
Mariotaku <mariotaku.lee@gmail.com>
2022-01-10 03:43:01 +0900

    added helper functions

--- pelican/themes/webosbrew/__init__.py
+++ pelican/themes/webosbrew/__init__.py
@@ -1,9 +1,17 @@
 from os import path
 
 
-def scss_dir():
-    return path.join(path.dirname(__file__), 'static', 'styles')
+def theme_dir():
+    return path.dirname(__file__)
 
 
 def templates_dir():
-    return path.join(path.dirname(__file__), 'templates')
+    return path.join(theme_dir(), 'templates')
+
+
+def static_dir():
+    return path.join(theme_dir(), 'static')
+
+
+def scss_dir():
+    return path.join(static_dir(), 'styles')
